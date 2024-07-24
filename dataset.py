9269b1add0c4ecf287ace22cf97cdcc8289677f1
Sungjae Cho <sungjae.cho@umontreal.ca>
2024-07-23 21:53:54 -0400

    Add CIFAR-10.

--- dataset.py
+++ dataset.py
@@ -12,7 +12,15 @@ def load_dataset(dataset, batch_size):
         testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=mnist_transforms)
         testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)   # Enable persistent_workers=True if more than 1 worker to save CPU
 
-    elif 'NSD': # Natural Scence Dataset
-        pass
+    elif 'CIFAR10': # CIFAR-10
+        # Reference: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+        transforms = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)  # Enable persistent_workers=True if more than 1 worker to save CPU
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)   # Enable persistent_workers=True if more than 1 worker to save CPU
 
     return (trainset, testset), (trainloader, testloader)
